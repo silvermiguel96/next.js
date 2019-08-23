5221494439b02010f0f7cf12cb926c217df4d880
silvermiguel96 <silvermiguel96@gmail.com>
2019-08-23 15:58:32 -0500

    Styled JSX

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
 export default class extends React.Component {
   render() {
-    return <h1>Hola mundo!</h1>
+    return <div>
+      <h1>Hola mundo!</h1>
+      <img src="/static/granate.png" alt="granate"/>
+      <p>Todos en el curso de Next.js</p>
+
+      <style jsx>{`
+      h1 {
+        color: red;
+      }
+      p {
+        color: green;
+      }
+      body {
+        background: yellow;
+      }
+      `}</style>
+
+      {/* <style jsx global>{`
+      body {
+        background: yellow;        
+      }
+      `}</style> */}
+    </div> 
   }
 }
