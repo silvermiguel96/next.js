0be7f488205162770b1defa5110fb80315ad6010
silvermiguel96 <silvermiguel96@gmail.com>
2019-09-05 10:19:28 -0500

    config next Routers

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "22c234330253be8a970c";
+/******/ 	var hotCurrentHash = "1c6655a231ae6cbc3d76";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
